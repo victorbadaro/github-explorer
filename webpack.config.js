72f1d9af9b3b5dc311b577df621f0e30fa8c2f28
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-03-10 01:05:43 -0300

    chore: add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -10,6 +11,12 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+            scriptLoading: 'blocking'
+        })
+    ],
     module: {
         rules: [
             {
